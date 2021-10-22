ce75d6e0533cc27e7de286eab0b245308f6e29b7
Daniel Perini <dsperini@gmail.com>
2021-10-22 18:36:41 -0300

    controllers implemented but still not ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    bookmark = Bookmark.new(task_params)
+    bookmark.save
+    redirect_to bookmarks_path
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,18 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to lists_path
+  end
+
+  def new
+    @list = List.new
+  end
 end
