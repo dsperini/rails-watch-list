f2e51b02a4e7fe405534a8bdaf8abb338fe0548a
Daniel Perini <dsperini@gmail.com>
2021-10-25 16:34:00 -0300

    forced implentation of controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,37 @@
 class BookmarksController < ApplicationController
+ before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
-    bookmark = Bookmark.new(bookmarks_params)
-    bookmark.save
-    redirect_to bookmarks_path
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
     @bookmark.destroy
-    redirect_to bookmarks_path
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
